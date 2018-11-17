5f82c11511e9e252851cf8a269299b1504e896ce
Peter Bakke <peter.polit@gmail.com>
2018-11-17 23:18:22 +0100

    3d plotting

--- Clustering.py
+++ Clustering.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from toolbox_02450 import clusterplot, clusterval
 from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
+from mpl_toolkits import mplot3d
 
 
 class HierarchicalCluster:
@@ -107,3 +108,19 @@ if __name__ == '__main__':
     myCluster.display_cluster_plot(max_cluster=2)
     myCluster.display_dendogram()
     myCluster.validate_cluster()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax = plt.axes(projection='3d')
+
+    # Data for a three-dimensional line
+    zline = np.linspace(0, 15, 1000)
+    xline = np.sin(zline)
+    yline = np.cos(zline)
+    ax.plot3D(xline, yline, zline, 'gray')
+
+    # Data for three-dimensional scattered points
+    zdata = 15 * np.random.random(100)
+    xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+    ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+    ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
+
