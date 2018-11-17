f46333f321a48bc555878ea86a65c397cbfff0ce
Peter Bakke <peter.polit@gmail.com>
2018-11-17 20:34:47 +0100

    controllers added

--- Project_3_main.py
+++ Project_3_main.py
@@ -8,6 +8,10 @@ from Clustering import HierarchicalCluster
 # Nicer formatting of plots
 sns.set_style("darkgrid")
 
+# Controllers
+set_feature_plot = False
+set_hierarchical_clustering = True
+
 # Creating object to handle data load and feature transformation
 myData = ProstateDataHandler.ProstateData()
 raw_data = myData.get_rawData()
@@ -21,28 +25,30 @@ C = len(classNames)
 N, M, X, y = myData.get_ClassificationFeatureData()
 
 # Plotting two features against each other
-ML_plotter.plot_attributes_2d(i=0,
-                              j=1,
-                              X=X,
-                              y=y,
-                              C=C,
-                              classNames=classNames,
-                              attributeNames=attributeNames,
-                              saveFigure=False)
+if set_feature_plot:
+    ML_plotter.plot_attributes_2d(i=0,
+                                j=1,
+                                X=X,
+                                y=y,
+                                C=C,
+                                classNames=classNames,
+                                attributeNames=attributeNames,
+                                saveFigure=False)
 
 
-# Creating a PCA object
-prostatePCA = report_PCA.prostatePCA(X=X)
+if set_hierarchical_clustering:
+    # Creating a PCA object
+    prostatePCA = report_PCA.prostatePCA(X=X)
 
-# Projecting the data down to the first two principal components (easier plotting)
-X_pca = prostatePCA.get_principal_components(n_pca=2)
+    # Projecting the data down to the first two principal components (easier plotting)
+    X_pca = prostatePCA.get_principal_components(n_pca=2)
 
-# Displaying the first two principal components against each other
-prostatePCA.display_pca()
+    # Displaying the first two principal components against each other
+    prostatePCA.display_pca()
 
-# Perform hierarchical clustering
-prostate_hierarchical_clustering = HierarchicalCluster(X=X_pca, y=y, method='average')
-prostate_hierarchical_clustering.display_cluster_plot(max_cluster=2)
-prostate_hierarchical_clustering.display_dendogram(max_display_levels=100)
+    # Perform hierarchical clustering
+    prostate_hierarchical_clustering = HierarchicalCluster(X=X_pca, y=y, method='average')
+    prostate_hierarchical_clustering.display_cluster_plot(max_cluster=2)
+    prostate_hierarchical_clustering.display_dendogram(max_display_levels=100)
 
 
