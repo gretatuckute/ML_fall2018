a3cc4486cf6f1683b06f2c628cd0bbeb44796931
Peter Bakke <peter.polit@gmail.com>
2018-11-27 16:57:16 +0100

    Update Project_3_main.py

--- Project_3_main.py
+++ Project_3_main.py
@@ -56,8 +56,8 @@ if set_hierarchical_clustering:
         prostatePCA.display_pca()
 
     # Perform hierarchical clustering
-    prostate_hierarchical_clustering = HierarchicalCluster(X=X, y=y, method='average')
-    prostate_hierarchical_clustering.display_cluster_plot(max_cluster=4)
+    prostate_hierarchical_clustering = HierarchicalCluster(X=X, y=y, method='complete')
+    prostate_hierarchical_clustering.display_cluster_plot(max_cluster=2)
     prostate_hierarchical_clustering.display_dendogram(max_display_levels=100, orientation='top', color_threshold=3.3)
     prostate_hierarchical_clustering.validate_cluster(K=10)
 
